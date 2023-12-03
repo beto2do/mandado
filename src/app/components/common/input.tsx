eb2e3a78bf2aed80e02266560eb2f6bc5953aebe
Humberto <humberto2do@gmail.com>
2023-12-02 21:09:02 -0800

    add input attributes

--- src/app/components/common/input.tsx
+++ src/app/components/common/input.tsx
@@ -1,12 +1,10 @@
-import { forwardRef, LegacyRef } from 'react';
+import { forwardRef, LegacyRef, AllHTMLAttributes } from 'react';
 
-const Input =  forwardRef(({placeholder, value, onChange}: any, ref:LegacyRef<HTMLInputElement> | undefined) => {
+const Input =  forwardRef((props: AllHTMLAttributes<HTMLInputElement>, ref:LegacyRef<HTMLInputElement> | undefined) => {
         return(
             <input
                 className='block w-full py-1.5 px-3 text-base font-normal leading-6 border-solid border border-zinc-300 rounded' 
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
+                {...props}
                 ref={ref}
             />
         );
