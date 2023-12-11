eabc8b0448818422609dbb6de90c002e7eac6f21
Humberto <humberto2do@gmail.com>
2023-12-11 14:25:37 -0800

    function calls only in server side

--- src/services/shopping.tsx
+++ src/services/shopping.tsx
@@ -1,8 +1,39 @@
+'use server'
 import Product from '@/models/product';
 
 export async function getShoppingList() : Promise<Product[]> {
-    const products = await fetch('/api/shop')
-                            .then((res) => res.json())
-                            .then((res) => res.data);
+    const products =  await new Promise<Product[]>(( resolve, reject)  => {
+        setTimeout(() => {
+            resolve(
+                [
+                    {
+                        id: '0',
+                        name:'Epazote',
+                        isGrabbed: true
+                    },
+                    {
+                        id: '1',
+                        name:'Tomates',
+                        isGrabbed: false
+                    },
+                    {
+                        id: '2',
+                        name:'Totopos',
+                        isGrabbed: true
+                    },
+                    {
+                        id: '3',
+                        name:'Bolillo',
+                        isGrabbed: false
+                    },
+                    {
+                        id: '4',
+                        name:'Yogurt',
+                        isGrabbed: true
+                    }
+                ]
+            )
+          }, 3000);
+    });
     return products;
 }
