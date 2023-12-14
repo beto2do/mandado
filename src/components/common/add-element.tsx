9e1342e7296254a5a51770d4e4dfee18b9c6e1ee
Humberto <humberto2do@gmail.com>
2023-12-13 23:31:35 -0800

    add validation for empty field

--- src/components/common/add-element.tsx
+++ src/components/common/add-element.tsx
@@ -5,19 +5,27 @@ import TextField from '@mui/material/TextField';
 
 export default function AddElement({onAdd}:{onAdd: any}) {
     const [text, setText] = useState('');
+    const [isEmpty, setIsEmpty] = useState(false);
     return (
     <div className="flex gap-1">
         <TextField
+            error={isEmpty}
             label="Add Element"
             value={text}
+            helperText={isEmpty ? 'Value is required' : '' }
             onChange={(e: any) => setText(e.target.value)}
         />
         <Button
             className='pointer-events-auto bg-red-600 text-white hover:bg-red-500 hover:border-0 border-0'
             variant="contained"
             onClick={() => {
-                setText('');
-                onAdd(text);
+                if(text === '') {
+                    setIsEmpty(true);
+                } else {
+                    setIsEmpty(false);
+                    setText('');
+                    onAdd(text);
+                }
             }}
         >Add</Button>
     </div>
