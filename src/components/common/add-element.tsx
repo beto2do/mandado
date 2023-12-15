d61071879eeabd000002dea671798d982f5547b2
Humberto <humberto2do@gmail.com>
2023-12-14 16:51:41 -0800

    add element in enter event

--- src/components/common/add-element.tsx
+++ src/components/common/add-element.tsx
@@ -6,6 +6,23 @@ import TextField from '@mui/material/TextField';
 export default function AddElement({onAdd}:{onAdd: any}) {
     const [text, setText] = useState('');
     const [isEmpty, setIsEmpty] = useState(false);
+
+    const addElement = () => {
+        if(text === '') {
+            setIsEmpty(true);
+        } else {
+            setIsEmpty(false);
+            setText('');
+            onAdd(text);
+        }
+    }
+
+    const pressEnter = (e: any) => {
+        if(e.type === 'keydown' && e.key === 'Enter') {
+            addElement();
+        }
+    }
+
     return (
     <div className="flex gap-1">
         <TextField
@@ -14,19 +31,12 @@ export default function AddElement({onAdd}:{onAdd: any}) {
             value={text}
             helperText={isEmpty ? 'Value is required' : '' }
             onChange={(e: any) => setText(e.target.value)}
+            onKeyDown={pressEnter}
         />
         <Button
             className='pointer-events-auto bg-red-600 text-white hover:bg-red-500 hover:border-0 border-0'
             variant="contained"
-            onClick={() => {
-                if(text === '') {
-                    setIsEmpty(true);
-                } else {
-                    setIsEmpty(false);
-                    setText('');
-                    onAdd(text);
-                }
-            }}
+            onClick={addElement}
         >Add</Button>
     </div>
     );
