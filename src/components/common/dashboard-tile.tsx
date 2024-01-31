e94c06d669df90b06683d2094edd63a0d0d6e520
Humberto <humberto2do@gmail.com>
2024-01-30 23:00:48 -0800

    improve tiles styles

--- src/components/common/dashboard-tile.tsx
+++ src/components/common/dashboard-tile.tsx
@@ -6,12 +6,13 @@ import Link from "next/link";
 import { CardActionArea, CardActions } from "@mui/material";
 import { getImage } from "@/services";
 import Image from "next/image";
+import Button from "@mui/material/Button";
 
 export async function DashboardTile({ tile }: { tile: Tile }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
-        <Image src={await getImage(tile.imgUrl)} alt="" />
+        <Image src={await getImage(tile.imgUrl)} alt="" width={345} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {tile.title}
@@ -22,7 +23,9 @@ export async function DashboardTile({ tile }: { tile: Tile }) {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Link href={tile.actionUrl}>{tile.action}</Link>
+        <Link href={tile.actionUrl}>
+          <Button color="primary">{tile.action}</Button>
+        </Link>
       </CardActions>
     </Card>
   );
--- src/components/common/logo-app.tsx
+++ src/components/common/logo-app.tsx
@@ -6,12 +6,18 @@ import { mobileDisplay, desktopDisplay } from "@/theme";
 export function LogoApp({ mobile = false }) {
   const breakpointStyle = mobile ? mobileDisplay : desktopDisplay;
   const growClass = mobile ? "grow sm:grow md:grow" : "";
-  const typographyClasses = `mr-4  font-bold tracking-widest text-inherit no-underline ${growClass}`;
+  const typographyClasses =
+    "mr-4  font-bold tracking-widest text-inherit no-underline";
 
   return (
     <>
       <StoreFront className={`mr-2`} sx={breakpointStyle} />
-      <Typography variant="h6" noWrap sx={breakpointStyle}>
+      <Typography
+        variant="h6"
+        noWrap
+        sx={breakpointStyle}
+        className={growClass}
+      >
         <Link className={typographyClasses} href="/">
           Man-dado
         </Link>
